3e81415af7d82b59e7a7618e6d242ce5a50b45f1
i5ting <i5ting@126.com>
2016-12-13 23:35:57 +0800

    refact, please ignore

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var dirs = {
   screenshots: 'public/build/screenshots'
 };
 
-gulp.task('useref', ['screenshot'], function(){
+gulp.task('useref', function(){
   var assets = $.useref.assets({
     searchPath: 'public'
   });
@@ -30,43 +30,4 @@ gulp.task('useref', ['screenshot'], function(){
     .pipe(gulp.dest('public'));
 });
 
-gulp.task('screenshot:rev', function(){
-  return gulp.src('public/themes/screenshots/*.png')
-    .pipe($.rev())
-    .pipe(gulp.dest(dirs.screenshots))
-    .pipe($.rev.manifest())
-    .pipe(gulp.dest(dirs.screenshots));
-});
-
-gulp.task('screenshot:resize', ['screenshot:rev'], function(){
-  return gulp.src(dirs.screenshots + '/*.png')
-    .pipe($.responsive({
-      '*.png': [
-        {
-          width: 400,
-          progressive: true
-        },
-        {
-          progressive: true,
-          rename: {
-            suffix: '@2x'
-          }
-        }
-      ]
-    }))
-    .pipe(gulp.dest(dirs.screenshots));
-});
-
-gulp.task('screenshot:revreplace', ['screenshot:rev'], function(){
-  return gulp.src([dirs.screenshots + '/rev-manifest.json', 'public/themes/index.html'])
-    .pipe($.revCollector({
-      replaceReved: true,
-      dirReplacements: {
-        '/themes/screenshots': '/build/screenshots'
-      }
-    }))
-    .pipe(gulp.dest('public/themes'));
-});
-
-gulp.task('screenshot', ['screenshot:rev', 'screenshot:resize', 'screenshot:revreplace']);
-gulp.task('default', ['useref', 'screenshot']);
+gulp.task('default', ['useref']);
